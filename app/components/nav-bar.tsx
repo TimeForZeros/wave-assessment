4ccd1a5bede4a2a39af1524dea16682681380749
Victor Gonzalez <dazedbutamused@gmail.com>
2025-07-31 23:24:59 -0700

    navbar

---
+++ app/components/nav-bar.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '../components/ui/navigation-menu';
+
+const NavBar = () => (
+  <header className='w-full flex justify-between bg-slate-700 py-3 sticky'>
+    <NavigationMenu viewport={false} className='mx-auto'>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink href='/' className='font-bold text-white'>Dashboard</NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink href='/add-user' className='font-bold text-white'>Add User</NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  </header>
+);
+export default NavBar
--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import type { Route } from './+types/root';
 import './app.css';
+import NavBar from './components/nav-bar';
 
 const queryClient = new QueryClient();
 
@@ -35,6 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
+      <NavBar />
       <body>
         {children}
         <ScrollRestoration />
